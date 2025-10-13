3bee247850d58166587ccec915041de60e2d1e27
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2025-10-13 20:54:05 +0100

    Create main.js
    Yes

---
+++ client/main.js
@@ -0,0 +1,22 @@
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>🚀 Mine App</title>
+  <link
+    href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&display=swap"
+    rel="stylesheet"
+  />
+  <style>
+    body {
+      margin: 0;
+      padding: 0;
+      background-color: #000;
+      color: #00ff88;
+      font-family: "Poppins", sans-serif;
+    }
+  </style>
+</head>
+
+<body>
+  <div id="app"></div>
+</body>
