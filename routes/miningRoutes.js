3f8b3db51333f244ed4f4a7f5f641cc2ed54de11
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2025-12-22 14:41:25 +0100

    Delete routes/miningRoutes.js
    Yes 
    
    Signed-off-by: HILL-TOP-web <oluwayimikadavid491@gmail.com>

--- routes/miningRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { mineSKD } from "../controllers/miningController.js";
-
-const router = express.Router();
-
-// /mine/:userId
-router.post("/:userId", mineSKD);
-
-export default router;
