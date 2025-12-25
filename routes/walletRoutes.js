9004dd72cde142830a778c756a5467bd9b76e5ea
HILL-TOP-web <oluwayimikadavid491@gmail.com>
2025-12-25 17:36:39 +0100

    Delete routes/walletRoutes.js
    Yes 
    
    Signed-off-by: HILL-TOP-web <oluwayimikadavid491@gmail.com>

--- routes/walletRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from "express";
-import { convertSKD } from "../controllers/walletController.js";
-
-const router = express.Router();
-
-// /wallet/convert
-router.post("/convert", convertSKD);
-
-export default router;
